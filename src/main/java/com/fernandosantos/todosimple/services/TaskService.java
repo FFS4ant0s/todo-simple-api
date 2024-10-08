345a3c3403e6098326eaf662cb90953b6600d25a
Fernando <silentguyband@gmail.com>
2024-10-08 14:28:39 -0300

    Create TaksController

--- src/main/java/com/fernandosantos/todosimple/services/TaskService.java
+++ src/main/java/com/fernandosantos/todosimple/services/TaskService.java
@@ -1,5 +1,6 @@
 package com.fernandosantos.todosimple.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,11 @@ public class TaskService {
         return task.orElseThrow(() -> new RuntimeException(
                 "Tarefa não encontrada! Id: " + id + ", Tipo: " + User.class.getName()));
 
+    }
+
+    public List<Task> findByUserId(Long userId) {
+        List<Task> tasks = this.taskRepository.findByUser_Id(userId);
+        return tasks;
     }
 
     @Transactional
