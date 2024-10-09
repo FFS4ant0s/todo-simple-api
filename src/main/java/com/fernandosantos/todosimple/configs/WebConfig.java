aae937db9a425acf2a88f7576a371261c1fb3763
Fernando <silentguyband@gmail.com>
2024-10-09 11:05:09 -0300

    Exception handling

--- src/main/java/com/fernandosantos/todosimple/configs/WebConfig.java
+++ src/main/java/com/fernandosantos/todosimple/configs/WebConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @SuppressWarnings("all")
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
